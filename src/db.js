5f1bf1a3f488b4fa02917f8fc01af7af0b8cff5c
Manuel Murillo <mmpeira@gmail.com>
2023-08-18 10:36:21 +0200

    db connected

---
+++ src/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config(); 
+
+
+const connectDB = async () => {
+    try {
+      await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+      console.log('>>> DB is connected')
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  export default connectDB;
