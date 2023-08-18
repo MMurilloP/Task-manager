af3ba47b0a6013a5aa626c9b19ed4bd3a40cce11
Manuel Murillo <mmpeira@gmail.com>
2023-08-18 18:06:33 +0200

    validateroutes with cookies

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import express from "express";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
 
 import authRouters from "./routes/auth.routes.js";
 
 const app = express();
 
 app.use(morgan("dev"));
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
-app.use('/api', authRouters);
+app.use("/api", authRouters);
 
 export default app;
