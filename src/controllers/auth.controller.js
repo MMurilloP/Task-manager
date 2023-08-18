aa80a27b6053300f0411737d5751e3fe381da764
Manuel Murillo <mmpeira@gmail.com>
2023-08-18 16:36:14 +0200

    login, register, logout working

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -31,6 +31,40 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send("login");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const userFound = await User.findOne({ email });
+    if (!userFound) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, userFound.password); //compara el password con el que busca en MongoDB
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Incorrect password" });
+    }
+
+    const token = await createAccessToken({ id: userFound._id });
+
+    res.cookie("token", token); // creo la cookie
+    // que queremos que nos devuelva, para poder usarlo en el front end. No neceistamos el password.
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updaeAt: userFound.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const logout = (req, res) => {
+  res.cookie("token", "", {
+    expires: new Date(0),
+  });
+  return res.sendStatus(200);
 };
